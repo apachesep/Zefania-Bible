f74e7397375d638db7f35e0307e736527c536ac7
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2012-12-31 20:28:17 -0500

    Added to GitHub

---
+++ com_zefaniabible/admin/views/zefaniabibleitem/tmpl/bibleadd_form.php
@@ -0,0 +1,154 @@
+<?php
+/**                               ______________________________________________
+*                          o O   |                                              |
+*                 (((((  o      <  Generated with Cook           (100% Vitamin) |
+*                ( o o )         |______________________________________________|
+* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
+* @version		1.6
+* @package		ZefaniaBible
+* @subpackage	Zefaniabible
+* @copyright	Missionary Church of Grace
+* @author		Andrei Chernyshev - www.missionarychurchofgrace.org - andrei.chernyshev1@gmail.com
+* @license		GNU/GPL
+*
+* /!\  Joomla! is free software.
+* This version may have been modified pursuant to the GNU General Public License,
+* and as distributed it includes or is derivative of works licensed under the
+* GNU General Public License or other free or open source software licenses.
+*
+*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
+*             (   )  (   )
+* -------------\ (----) /----------------------------------------------------------- +
+*               \_)  (_/
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+
+
+$isNew		= ($this->zefaniabibleitem->id < 1);
+$actionText = $isNew ? JText::_( "ZEFANIABIBLE_NEW" ) : JText::_( "ZEFANIABIBLE_EDIT" );
+?>
+
+<fieldset class="fieldsform">
+	<legend><?php echo $actionText;?></legend>
+
+	<table class="admintable">
+
+		<tr>
+			<td align="right" class="key">
+				<label for="title">
+					<?php echo JText::_( "ZEFANIABIBLE_FIELD_TITLE" ); ?> :
+				</label>
+			</td>
+			<td>
+				<?php echo JDom::_('html.form.input.text', array(
+												'dataKey' => 'bible_name',
+												'dataObject' => $this->zefaniabibleitem,
+												'size' => "32"
+												));
+				?>
+			</td>
+		</tr>
+		<tr>
+			<td align="right" class="key">
+				<label for="alias">
+					<?php echo JText::_( "ZEFANIABIBLE_FIELD_ALIAS" ); ?> :
+				</label>
+			</td>
+			<td>
+				<?php echo JDom::_('html.form.input.text', array(
+												'dataKey' => 'alias',
+												'dataObject' => $this->zefaniabibleitem,
+												'size' => "32"
+												));
+				?>
+			</td>
+		</tr>
+		<tr>
+			<td align="right" class="key">
+				<label for="full_name">
+					<?php echo JText::_( "ZEFANIABIBLE_FIELD_FULL_NAME" ); ?> :
+				</label>
+			</td>
+			<td>
+				<?php echo JDom::_('html.form.input.text', array(
+												'dataKey' => 'desc',
+												'dataObject' => $this->zefaniabibleitem,
+												'size' => "32"
+												));
+				?>
+			</td>
+		</tr>
+		<?php if ($this->access->get('core.edit.state')): ?>
+		<tr>
+			<td align="right" class="key">
+				<label for="publish">
+					<?php echo JText::_( "ZEFANIABIBLE_FIELD_PUBLISH" ); ?> :
+				</label>
+			</td>
+			<td>
+				<?php echo JDom::_('html.form.input.bool', array(
+												'dataKey' => 'publish',
+												'dataObject' => $this->zefaniabibleitem,
+												'aclAccess' => 'core.edit.state'
+												));
+				?>
+			</td>
+		</tr>
+
+		<?php endif; ?>
+		<?php if ($this->access->get('core.edit') || $this->access->get('core.edit.state')): ?>
+		<tr>
+			<td align="right" class="key">
+				<label for="ordering">
+					<?php echo JText::_( "ZEFANIABIBLE_FIELD_ORDERING" ); ?> :
+				</label>
+			</td>
+			<td>
+				<?php echo JDom::_('html.form.input.ordering', array(
+												'dataKey' => 'ordering',
+												'dataObject' => $this->zefaniabibleitem,
+												'items' => $this->lists["ordering"],
+												'labelKey' => 'bible_name',
+												'aclAccess' => '',
+												'validatorHandler' => "numeric"
+												)); ?>
+			</td>
+		</tr>
+
+		<?php endif; ?>
+		<tr>
+			<td align="right" class="key">
+				<label for="file_location">
+					<?php echo JText::_( "ZEFANIABIBLE_FIELD_XML_BIBLE_FILE_LOCATION" ); ?> :
+				</label>
+			</td>
+			<td>
+				<?php echo JDom::_('html.form.input.text', array(
+												'dataKey' => 'xml_file_url',
+												'dataObject' => $this->zefaniabibleitem,
+												'size' => "32"
+												));
+				?>
+			</td>
+		</tr>
+		<tr>
+			<td align="right" class="key">
+				<label for="xml_audio_file_location">
+					<?php echo JText::_( "ZEFANIABIBLE_FIELD_XML_AUDIO_FILE_LOCATION" ); ?> :
+				</label>
+			</td>
+			<td>
+				<?php echo JDom::_('html.form.input.text', array(
+												'dataKey' => 'xml_audio_url',
+												'dataObject' => $this->zefaniabibleitem,
+												'size' => "32"
+												));
+				?>
+			</td>
+		</tr>
+
+
+	</table>
+</fieldset>
