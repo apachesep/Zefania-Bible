69d3d6b41a2d4c9227401d2304e1060ebabaa283
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2012-12-31 22:16:29 -0500

    updated code

---
+++ com_zefaniabible/admin/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,18 @@
+
+DROP TABLE IF EXISTS `#__zefaniabible_zefaniareading`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_zefaniareadingdetails`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_zefaniauser`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_zefaniaverseofday`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_zefaniabiblebooknames`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_zefaniapublish`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_bible_names`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_bible_text`;
+
+DROP TABLE IF EXISTS `#__zefaniabible_zefaniacomment`;
