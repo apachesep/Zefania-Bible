43a397b1b34552913fdf35786b3656dd295b37db
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2012-12-31 20:27:59 -0500

    Added to GitHub

---
+++ com_zefaniabible/admin/views/zefaniabiblebooknames/tmpl/default.php
@@ -0,0 +1,96 @@
+<?php
+/**                               ______________________________________________
+*                          o O   |                                              |
+*                 (((((  o      <  Generated with Cook           (100% Vitamin) |
+*                ( o o )         |______________________________________________|
+* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
+* @version		1.6
+* @package		ZefaniaBible
+* @subpackage	Zefaniabiblebooknames
+* @copyright	Missionary Church of Grace
+* @author		Andrei Chernyshev - www.missionarychurchofgrace.org - andrei.chernyshev1@gmail.com
+* @license		GNU/GPL
+*
+* /!\  Joomla! is free software.
+* This version may have been modified pursuant to the GNU General Public License,
+* and as distributed it includes or is derivative of works licensed under the
+* GNU General Public License or other free or open source software licenses.
+*
+*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
+*             (   )  (   )
+* -------------\ (----) /----------------------------------------------------------- +
+*               \_)  (_/
+*/
+
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<?php ZefaniabibleHelper::headerDeclarations(); ?>
+
+
+<?php JHTML::_('behavior.tooltip');?>
+<?php JHTML::_('behavior.calendar');?>
+
+
+<?php
+	JToolBarHelper::title(JText::_("ZEFANIABIBLE_LAYOUT_BIBLE_BOOKS"), 'zefaniabible_zefaniabiblebooknames' );
+	$this->token = JUtility::getToken();
+?>
+
+<script language="javascript" type="text/javascript">
+	function submitbutton(pressbutton)
+	{
+		switch(pressbutton)
+		{
+			case 'delete':
+
+				var deleteConfirmMessage;
+				if (document.adminForm.boxchecked.value > 1)
+					deleteConfirmMessage = "<?php echo(addslashes(JText::_("JDOM_ALERT_ASK_BEFORE_REMOVE_MULTIPLE"))); ?>";
+				else
+					deleteConfirmMessage = "<?php echo(addslashes(JText::_("JDOM_ALERT_ASK_BEFORE_REMOVE"))); ?>";
+
+				if (window.confirm(deleteConfirmMessage))
+					return Joomla.submitform(pressbutton);
+				else
+					return;
+				break;
+
+		}
+
+		return Joomla.submitform(pressbutton);
+	}
+</script>
+
+<form action="<?php echo(JRoute::_("index.php")); ?>" method="post" name="adminForm" id="adminForm" class="">
+
+
+
+
+
+	<div>
+		<?php echo $this->loadTemplate('filters'); ?>
+		<?php echo $this->loadTemplate('grid'); ?>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+	<?php echo JDom::_('html.form.footer', array(
+		'values' => array(
+				'option' => "com_zefaniabible",
+				'view' => "zefaniabiblebooknames",
+				'layout' => "default",
+				'boxchecked' => "0",
+				'filter_order' => $this->lists['order'],
+				'filter_order_Dir' => $this->lists['order_Dir']
+			)));
+	?>
+
+</form>
