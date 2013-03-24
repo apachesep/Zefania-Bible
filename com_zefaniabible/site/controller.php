4ca790fc155d6794f7d8fd59d8ccd197f1d92a17
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2013-03-24 18:47:25 -0400

    Added

---
+++ com_zefaniabible/site/controller.php
@@ -0,0 +1,50 @@
+<?php
+/**                               ______________________________________________
+*                          o O   |                                              |
+*                 (((((  o      <  Generated with Cook           (100% Vitamin) |
+*                ( o o )         |______________________________________________|
+* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
+* @version		1.6
+* @package		ZefaniaBible
+* @subpackage	Zefaniabiblebooknames
+* @copyright	Missionary Church of Grace
+* @author		Andrei Chernyshev - www.missionarychurchofgrace.org - andrei.chernyshev1@gmail.com
+* @license		GNU/GPL
+*
+* /!\  Joomla! is free software.
+* This version may have been modified pursuant to the GNU General Public License,
+* and as distributed it includes or is derivative of works licensed under the
+* GNU General Public License or other free or open source software licenses.
+*
+*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
+*             (   )  (   )
+* -------------\ (----) /----------------------------------------------------------- +
+*               \_)  (_/
+*/
+
+
+// No direct access
+defined('_JEXEC') or die;
+
+class ZefaniabibleController extends JControllerLegacy
+{
+	/**
+	 * Method to display a view.
+	 *
+	 * @param	boolean			$cachable	If true, the view output will be cached
+	 * @param	array			$urlparams	An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+	 *
+	 * @return	JController		This object to support chaining.
+	 * @since	1.5
+	 */
+	public function display($cachable = false, $urlparams = false)
+	{
+
+		$view		= JFactory::getApplication()->input->getCmd('view', 's');
+        JFactory::getApplication()->input->set('view', $view);
+		
+		$urlparams = array('option'=>'STRING', 'view'=>'STRING', 'layout'=>'STRING', 'Itemid'=>'INT', 'tmpl'=>'STRING', 'lang'=>'CMD', 'a'=>'STRING','b'=>'STRING','c'=>'STRING','d'=>'STRING','e'=>'STRING','f'=>'STRING','g'=>'STRING','h'=>'STRING');
+		
+		return parent::display($cachable, $urlparams);
+	}
+}
