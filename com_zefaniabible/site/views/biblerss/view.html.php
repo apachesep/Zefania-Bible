4534b4722cc2c50415a2425809ab260f9b6e74dc
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2013-09-14 22:12:17 -0400

    New RSS Feed View

---
+++ com_zefaniabible/site/views/biblerss/view.html.php
@@ -0,0 +1,85 @@
+<?php
+
+/**                               ______________________________________________
+*                          o O   |                                              |
+*                 (((((  o      <  Generated with Cook           (100% Vitamin) |
+*                ( o o )         |______________________________________________|
+* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
+* @version		1.6
+* @package		ZefaniaBible
+* @subpackage	Zefaniabible
+* @copyright	Missionary Church of Grace
+* @author		Andrei Chernyshev - www.missionarychurchofgrace.org - andrei.chernyshev1@gmail.com
+* @license		GNU/GPL
+*
+* /!\  Joomla! is free software.
+* This version may have been modified pursuant to the GNU General Public License,
+* and as distributed it includes or is derivative of works licensed under the
+* GNU General Public License or other free or open source software licenses.
+*
+*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
+*             (   )  (   )
+* -------------\ (----) /----------------------------------------------------------- +
+*               \_)  (_/
+*/
+
+
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view');
+jimport( '0');
+
+/**
+ * HTML View class for the Zefaniabible component
+ *
+ * @static
+ * @package		Joomla
+ * @subpackage	Zefaniabible
+ *
+ */
+class ZefaniabibleViewBiblerss extends JViewLegacy
+{
+	/*
+	 * Define here the default list limit
+	 */
+	protected $_default_limit = null;
+
+	function display($tpl = null)
+	{
+		$app = JFactory::getApplication();
+		$config = JFactory::getConfig();
+		$option	= JRequest::getCmd('option');
+		$view	= JRequest::getCmd('view');
+		$layout = $this->getLayout();
+		switch($layout)
+		{
+			case 'default':
+				$fct = "display_" . $layout;
+				$this->$fct($tpl);
+				break;
+		}
+	}
+	function display_default($tpl = null)
+	{
+		/*
+			a = bible alias
+			b = book id
+			c = chapter number
+		*/		
+		$app = JFactory::getApplication();
+		$option	= JRequest::getCmd('option');
+		$user 	= JFactory::getUser();
+		
+		$this->params = JComponentHelper::getParams( 'com_zefaniabible' );
+		$str_primary_bible = 		$this->params->get('primaryBible', 'kjv');	
+		$str_Bible_Version = JRequest::getCmd('a', $str_primary_bible);	
+		$int_book_id = JRequest::getInt('b', 1);
+		$int_chapter_id = JRequest::getInt('c', 1);
+
+		header('HTTP/1.1 301 Moved Permanently');
+		header('Location: '.JURI::root().'index.php?option=com_zefaniabible&view=biblerss&format=raw&a='.$str_Bible_Version."&b=".$int_book_id.'&c='.$int_chapter_id);	
+		parent::display($tpl);
+	}
+}
