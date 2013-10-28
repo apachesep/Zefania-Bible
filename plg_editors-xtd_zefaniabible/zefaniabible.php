59834cdb0c819844be65118b5e30e6f3de92e456
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2013-10-27 20:39:26 -0400

    Redoing button setup

--- plg_editors-xtd_zefaniabible/zefaniabible.php
+++ plg_editors-xtd_zefaniabible/zefaniabible.php
@@ -19,22 +19,23 @@ class plgButtonZefaniabible extends JPlugin
 			jInsertEditorText(tag, '".$name."');
 			SqueezeBox.close();
 		}";
-
+		
 		$doc = JFactory::getDocument();
-		$doc->addScriptDeclaration($js);
+		$doc->addScriptDeclaration($js);		
+		$doc->addStyleSheet('/administrator/components/com_zefaniabible/css/zefaniabible.css'); 
 		/*
 		 * Use the built-in element view to select the sermon.
 		 * Currently uses blank class for Jooml 2.5 compatibility.
 		 */
 		$link = 'index.php?option=com_zefaniabible&amp;view=zefaniamodal&amp;tmpl=component&amp;'.JSession::getFormToken().'=1';
-
+		JHtml::_('behavior.modal');
 		$button = new JObject();
-		$button->set('modal', true);
-		$button->set('link', $link);
-		$button->set('text', JText::_('PLG_EDITORS-XTD_ZEFANIABIBLE_BUTTON_ZEFANIABIBLE'));
-		$button->set('name', 'broadcast blank');
-		$button->set('options', "{handler: 'iframe', size: {x: 770, y: 400}}");
-
+		$button->modal = true;
+		$button->link = $link;
+		$button->class = 'btn';
+		$button->text = JText::_('PLG_EDITORS-XTD_ZEFANIABIBLE_BUTTON_ZEFANIABIBLE');
+		$button->name = 'file-add';
+		$button->options = "{handler: 'iframe', size: {x: 770, y: 550}}";
 		return $button;
 	}
 }
