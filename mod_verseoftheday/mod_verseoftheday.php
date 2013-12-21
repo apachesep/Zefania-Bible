f6c686e6684152d0eb0338af8007d58ad65d634f
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2013-12-21 07:21:19 -0500

    Bug Fix

--- mod_verseoftheday/mod_verseoftheday.php
+++ mod_verseoftheday/mod_verseoftheday.php
@@ -62,23 +62,23 @@ class ZefVerseOfTheDay
 		$this->str_start_date = new DateTime($params->get('start_date'));	
 		// time zone offset.
 		$config = JFactory::getConfig();
-		date_default_timezone_set($config->get('offset'));	
+		//date_default_timezone_set($config->get('offset'));	
 
-		$this->str_today = new DateTime(date('Y-m-d'));
+		$this->str_today = new DateTime(date('d-m-Y'));
 		$this->int_day_diff = round(abs($this->str_today->format('U') - $this->str_start_date->format('U')) / (60*60*24));	
 		
 		$this->fnc_Get_Verse_Of_The_Day_Info();
 		if($this->int_display_order == 0)
 		{
-			$this->int_verse_remainder = $this->int_day_diff % $this->int_max_verses -1;
+			$this->int_verse_remainder = $this->int_day_diff % ($this->int_max_verses-1);
 		}
 		else
 		{
-			$this->int_verse_remainder = mt_rand(1, $this->int_max_verses);
+			$this->int_verse_remainder = mt_rand(1, $this->int_max_verses-1);
 		}
-		if($this->int_verse_remainder <= 0)
+		if($this->int_verse_remainder == 0)
 		{
-			$this->int_verse_remainder = $this->int_max_verses -1;
+			$this->int_verse_remainder = $this->int_max_verses-1 ;
 		}
 		$this->params = JComponentHelper::getParams( 'com_zefaniabible' );
 		$this->biblePath = $this->params->get('xmlBiblesPath', 'media/com_zefaniabible/bibles/');
