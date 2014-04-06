758f6e111d91b17cc063bfa19f77e24660c93c19
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2014-04-06 08:31:54 -0400

    Update Credits

--- com_zefaniabible/site/helpers/credits.php
+++ com_zefaniabible/site/helpers/credits.php
@@ -28,7 +28,7 @@ class ZefaniabibleCredits
 	{
 		echo '<div class="zef_credits">';
 		echo JText::_('ZEFANIABIBLE_DEVELOPED_BY')." <a href='http://www.zefaniabible.com/?utm_campaign=".JRequest::getCmd('view')."&utm_medium=referral&utm_source=".substr(JURI::base(),7,-1)."' target='_blank'>Zefania Bible</a>"; 
-		echo "<br><a href='http://www.missionarychurchofgrace.org/?utm_campaign=".JRequest::getCmd('view')."&utm_medium=referral&utm_source=".substr(JURI::base(),7,-1)."' target='_blank'>".JText::_('ZEFANIABIBLE_DEVELOPER_CREDIT')."</a>"; 
+		echo "<br><a href='http://www.propoved.org/?utm_campaign=".JRequest::getCmd('view')."&utm_medium=referral&utm_source=".substr(JURI::base(),7,-1)."' target='_blank'>".JText::_('ZEFANIABIBLE_DEVELOPER_CREDIT')."</a>"; 
 		echo '</div>';
 
 	}
