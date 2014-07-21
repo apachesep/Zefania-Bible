fc493fb1d152c02e12018d577e6c1299166fd32e
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2014-07-21 10:49:14 -0400

    Added

--- com_zefaniabible/site/models/default.php
+++ com_zefaniabible/site/models/default.php
@@ -92,7 +92,148 @@ class ZefaniabibleModelDefault extends JModelItem
 			$this->setError($e);
 		}
 		return $data;			
-	} 	 
+	}
+	function _buildQuery_References($item)
+	{
+		try 
+		{
+			$db = $this->getDbo();
+			$query  = $db->getQuery(true);
+			$query->select('a.verse_id');
+			$query->from('`#__zefaniabible_crossref` AS a');	
+			$query->where("a.book_id=".$item->int_Bible_Book_ID);
+			$query->where("a.chapter_id=".$item->int_Bible_Chapter);
+			$query->order('a.verse_id, a.sort_order');		
+			$db->setQuery($query);
+			$data = $db->loadObjectList();	
+		}
+		catch (JException $e)
+		{
+			$this->setError($e);
+		}
+		return $data;
+	}
+	function _buildQuery_commentary_list()
+	{
+		try 
+		{
+			$db = $this->getDbo();
+			$query  = $db->getQuery(true);
+			$query->select('title,alias');
+			$query->from('`#__zefaniabible_zefaniacomment`');
+			$query->where('publish=1');
+			$query->order('title');
+			$db->setQuery($query);
+			$data = $db->loadObjectList();
+		}
+		catch (JException $e)
+		{
+			$this->setError($e);
+		}
+		return $data;	
+	}		
+	function _buildQuery_commentary_chapter($str_commentary,$int_Bible_Book_ID,$int_Bible_Chapter)
+	{
+		try 
+		{
+			$db = $this->getDbo();
+			$query  = $db->getQuery(true);
+			$query->select('a.verse_id');
+			$query->from('`#__zefaniabible_comment_text` AS a');
+			$query->innerjoin('`#__zefaniabible_zefaniacomment` AS b ON a.bible_id = b.id');
+			$query->where('b.alias="'.$str_commentary.'"');
+			$query->where('a.book_id='.$int_Bible_Book_ID);
+			$query->where('a.chapter_id='.$int_Bible_Chapter);
+			$query->order('a.verse_id');
+			$db->setQuery($query);
+			$data = $db->loadObjectList();
+		}
+		catch (JException $e)
+		{
+			$this->setError($e);
+		}
+		return $data;	
+	}		
+	function _buildQuery_Bibles_Names()
+	{
+		try 
+		{
+			$db = $this->getDbo();
+			$query  = $db->getQuery(true);
+			$query->select('alias, bible_name');
+			$query->from('`#__zefaniabible_bible_names`');	
+			$query->where("publish = 1");
+			$query->order('bible_name');			
+			$db->setQuery($query);
+			$data = $db->loadObjectList();			
+		}
+		catch (JException $e)
+		{
+			$this->setError($e);
+		}
+		return $data;		
+	}	
+	function _buildQuery_Chapter($int_Bible_Chapter,$int_Bible_Book_ID,$str_Bible_Version)
+	{
+		try 
+		{
+			$db = $this->getDbo();
+			$query  = $db->getQuery(true);
+			$query->select('a.verse_id, a.verse');
+			$query->from('`#__zefaniabible_bible_text` AS a');
+			$query->innerJoin("`#__zefaniabible_bible_names` AS b ON a.bible_id = b.id");
+			$query->where("a.chapter_id=".$int_Bible_Chapter);
+			$query->where("a.book_id=".$int_Bible_Book_ID);
+			$query->where("b.alias='".$str_Bible_Version."'");
+			$query->order('a.verse_id ASC');			
+			$db->setQuery($query);
+			$data = $db->loadObjectList();			
+		}
+		catch (JException $e)
+		{
+			$this->setError($e);
+		}
+		return $data;		
+	}	
+	function _buildQuery_Max_Verse($item)
+	{
+		try 
+		{
+			$db = $this->getDbo();
+			$query  = $db->getQuery(true);
+			$query->select('Max(verse_id)');
+			$query->from('`#__zefaniabible_bible_text`');	
+			$query->where("book_id=".$item->int_Bible_Book_ID);
+			$query->where("chapter_id=".$item->int_Bible_Chapter);				
+			$db->setQuery($query);
+			$data = $db->loadResult();
+		}
+		catch (JException $e)
+		{
+			$this->setError($e);
+		}
+		return $data;			
+	}
+
+	function _buildQuery_Max_Chapter($item)
+	{
+		try 
+		{
+			$db = $this->getDbo();
+			$query  = $db->getQuery(true);
+			$query->select('Max(chapter_id)');
+			$query->from('`#__zefaniabible_bible_text`');	
+			$query->where("book_id=".$item->int_Bible_Book_ID);			
+			$db->setQuery($query);
+			$data = $db->loadResult();			
+		}
+		catch (JException $e)
+		{
+			$this->setError($e);
+		}
+		return $data;		
+	}
+			
 	function _buildQuery_first_record()
 	{
 		try 
