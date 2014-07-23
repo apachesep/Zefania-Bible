560e93bbf52072512ac46bf9b80ecc0390986aec
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2014-07-23 09:18:05 -0400

    Cleaned code

--- com_zefaniabible/site/views/references/tmpl/default.php
+++ com_zefaniabible/site/views/references/tmpl/default.php
@@ -1,24 +1,3 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
-JHTML::addIncludePath(JPATH_COMPONENT.'/helpers');
-?>
-
-<?
-class BibleReference
-{
-
-	public function __construct()
-	{
-		/*
-			a = commentary
-			b = book
-			c = chapter
-			d = verse	
-		*/		
-
-	}
-}
-
- $cls_Bible_reference = new BibleReference();
-
 ?>
