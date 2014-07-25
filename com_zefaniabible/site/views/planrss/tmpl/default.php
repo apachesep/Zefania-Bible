ebffc49f3fad2cb35789f53eb34de4294c899f0a
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2014-07-25 15:15:55 -0400

    Rewrote the code

--- com_zefaniabible/site/views/planrss/tmpl/default.php
+++ com_zefaniabible/site/views/planrss/tmpl/default.php
@@ -24,11 +24,11 @@
 
 defined('_JEXEC') or die('Restricted access'); ?>
 <?php 
-$cls_PlanRSS = new PlanRss($this->str_Bible_Name, $this->arr_Plan_Info); 
+$cls_PlanRSS = new PlanRss($this->item); 
 class PlanRss 
 {
 
-	public function __construct($str_Bible_Name, $arr_Plan_Info)
+	public function __construct($item)
 	{
 		/*
 			a = Plan Alias
@@ -37,53 +37,38 @@ class PlanRss
 			d = number of items
 			e = feed type atom/rss
 		*/			
-		$this->params = JComponentHelper::getParams( 'com_zefaniabible' );
+		$params = JComponentHelper::getParams( 'com_zefaniabible' );
 		$mainframe = JFactory::getApplication();
-		$str_primary_bible = 		$this->params->get('primaryBible', 'kjv');
-		$str_default_image = 		$this->params->get('str_default_image', 'media/com_zefaniabible/images/bible_100.jpg');
-		$str_primary_plan = 		$this->params->get('primaryReading');
-		$str_plan_alias = 	JRequest::getCmd('a', $str_primary_plan);	
-		$str_Bible_Version = JRequest::getCmd('b', $str_primary_bible);	
-		$int_start_item = JRequest::getInt('c', JRequest::getVar('limitstart', 0, '', 'int'));
-		$int_number_of_items = JRequest::getInt('d', $mainframe->getCfg('feed_limit'));
-		$str_feed_type = JRequest::getCmd('e', 'rss');	
-		
 		$doc = JFactory::getDocument();
-		$mainframe = JFactory::getApplication();
-		$str_menuItem = $this->params->get('rp_mo_menuitem', 0);										
+		
+		$str_menuItem = $params->get('rp_mo_menuitem', 0);										
 		$str_url_link = '';	
 		$str_url_escaped = 	str_replace('&', '&amp;',$str_url_link);
-		$str_admin_email = $this->params->get('adminEmail', 'admin@'.substr(JURI::root(),7,-1));
-		$str_plan_name = '';
-		foreach ($arr_Plan_Info as $obj_plan_info)
-		{
-			$str_plan_name = $obj_plan_info->plan_name;
-			$str_plan_desc = JText::_($obj_plan_info->plan_desc);
-		}
-		if($str_feed_type == 'rss')
+		$str_admin_email = $params->get('adminEmail', 'admin@'.substr(JURI::root(),7,-1));
+		if($item->str_feed_type == 'rss')
 		{	
 			echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 			echo '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">'.PHP_EOL;
 			echo '<channel>'.PHP_EOL;
 			echo '	<atom:link href="'.htmlspecialchars(JURI::getInstance()).'" rel="self" type="application/rss+xml" />'.PHP_EOL;
 			echo '	<image>'.PHP_EOL;
-			echo '	  <url>'.JURI::root().$str_default_image.'</url>'.PHP_EOL;
-			echo '	  <title>'.$str_Bible_Name.'</title>'.PHP_EOL;
+			echo '	  <url>'.JURI::root().$item->str_default_image.'</url>'.PHP_EOL;
+			echo '	  <title>'.$item->str_bible_name.'</title>'.PHP_EOL;
 			echo '	  <link>'.JRoute::_(JURI::base().$str_url_escaped).'</link>'.PHP_EOL;
 			echo '	</image>'.PHP_EOL;		
-			echo '	<title>'.$str_plan_name.' - '.$str_Bible_Name.'</title>'.PHP_EOL;
+			echo '	<title>'.$item->str_reading_plan_name.' - '.$item->str_bible_name.'</title>'.PHP_EOL;
 			echo '	<link>'.JRoute::_(JURI::base().$str_url_escaped).'</link>'.PHP_EOL;			
 			echo '	<generator>Zefania Bible</generator>'.PHP_EOL;
 			echo '	<language>'.$doc->getLanguage().'</language>'.PHP_EOL;
 			echo '	<copyright>'.$mainframe->getCfg('sitename').'</copyright>'.PHP_EOL;
 			echo '	<description>';
-			echo 		$str_plan_desc;
+			echo 		$item->str_description;
 			echo '</description>'.PHP_EOL;
 			$x = 0;
-			foreach ($arr_Plan_Info as $obj_plan_info)
+			foreach ($item->arr_reading as $obj_plan_info)
 			{
 				$str_subtitle = '';
-				$str_link = JRoute::_(JURI::base()."index.php?option=com_zefaniabible&view=reading&a=".$str_plan_alias."&b=".$str_Bible_Version."&c=".$obj_plan_info->day_number);
+				$str_link = JRoute::_(JURI::base()."index.php?option=com_zefaniabible&view=reading&plan=".$item->str_reading_plan."&bible=".$item->str_Bible_Version."&day=".$obj_plan_info->day_number);
 				$str_url_escaped = 	str_replace('&', '&amp;',$str_link);
 				$str_subtitle = JText::_('ZEFANIABIBLE_BIBLE_BOOK_NAME_'.$obj_plan_info->book_id).' '.$obj_plan_info->begin_chapter;
 				if($obj_plan_info->begin_verse != 0)
@@ -117,18 +102,18 @@ class PlanRss
 		{
 			echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 			echo '<feed xmlns="http://www.w3.org/2005/Atom">'.PHP_EOL;
-        	echo '	<title>'.$str_plan_name.' - '.$str_Bible_Name.'</title>'.PHP_EOL;
-			echo '  <subtitle>'.$str_plan_desc.'</subtitle>'.PHP_EOL;
+        	echo '	<title>'.$item->str_reading_plan_name.' - '.$item->str_bible_name.'</title>'.PHP_EOL;
+			echo '  <subtitle>'.$item->str_description.'</subtitle>'.PHP_EOL;
 			echo '  <link href="'.htmlspecialchars(JURI::getInstance()).'" rel="self" />'.PHP_EOL;
 			echo '  <link href="'.JRoute::_(JURI::base().$str_url_escaped).'" />'.PHP_EOL;
 			echo '  <id>tag:'.substr(JURI::root(),7,-1).','.date('Y-m-d').':'.date('Ymd').'</id>'.PHP_EOL;
 			echo '  <updated>'.date('Y-m-d\TH:i:sP').'</updated>'.PHP_EOL;
 			$x = 0;
 			$y = time ();
-			foreach ($arr_Plan_Info as $obj_plan_info)
+			foreach ($item->arr_reading as $obj_plan_info)
 			{
 				$str_subtitle = '';
-				$str_link = JRoute::_(JURI::base()."index.php?option=com_zefaniabible&view=reading&a=".$str_plan_alias."&b=".$str_Bible_Version."&c=".$obj_plan_info->day_number);
+				$str_link = JRoute::_(JURI::base()."index.php?option=com_zefaniabible&view=reading&plan=".$item->str_reading_plan."&bible=".$item->str_Bible_Version."&day=".$obj_plan_info->day_number);
 				$str_url_escaped = 	str_replace('&', '&amp;',$str_link);
 				$str_subtitle = JText::_('ZEFANIABIBLE_BIBLE_BOOK_NAME_'.$obj_plan_info->book_id).' '.$obj_plan_info->begin_chapter;
 				if($obj_plan_info->begin_verse != 0)
