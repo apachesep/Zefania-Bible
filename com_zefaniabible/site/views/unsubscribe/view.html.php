45f86276aa168839614dd8c1eb63c85feb9d641b
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2014-08-05 09:56:47 -0400

    Rewrote Code

--- com_zefaniabible/site/views/unsubscribe/view.html.php
+++ com_zefaniabible/site/views/unsubscribe/view.html.php
@@ -29,8 +29,6 @@
 defined('_JEXEC') or die( 'Restricted access' );
 
 jimport( 'joomla.application.component.view');
-jimport( '0');
-
 /**
  * HTML View class for the Zefaniabible component
  *
@@ -49,9 +47,6 @@ class ZefaniabibleViewUnsubscribe extends JViewLegacy
 	function display($tpl = null)
 	{
 		$app = JFactory::getApplication();
-		$config = JFactory::getConfig();
-		$option	= JRequest::getCmd('option');
-		$view	= JRequest::getCmd('view');
 		$layout = $this->getLayout();
 		switch($layout)
 		{
@@ -65,20 +60,47 @@ class ZefaniabibleViewUnsubscribe extends JViewLegacy
 	function display_default($tpl = null)
 	{
 		$app = JFactory::getApplication();
-		$option	= JRequest::getCmd('option');
 		$user 	= JFactory::getUser();
-		$document	= JFactory::getDocument();
-
-		// create pagination
-		jimport('joomla.html.pagination');
+		$params = JComponentHelper::getParams( 'com_zefaniabible' );
+		$config = JFactory::getConfig();
 		
+		require_once(JPATH_COMPONENT_SITE.'/models/default.php');
+		require_once(JPATH_COMPONENT_SITE.'/helpers/common.php');
+		$mdl_default 	= new ZefaniabibleModelDefault;
+		$mdl_common 	= new ZefaniabibleCommonHelper;
+
+		$jinput = JFactory::getApplication()->input;
+		$item = new stdClass();
+		$item->str_email 						=	$jinput->get('email', $user->email, 'STRING');
+		$item->str_user_name 					=	$jinput->get('name', $user->name, 'USERNAME'); 
+		$item->flg_send_reading 				= 	$jinput->get('send_reading', 0, 'BOOL');
+		$item->flg_send_verse					=	$jinput->get('send_verse', 0, 'BOOL');
+		$item->str_view 						= 	$jinput->get('view', 'unsubscribe', 'CMD');
+		
+		$item->flg_use_catcha 					= 	$params->get('flg_use_catcha', '0');	
+		$item->str_admin_email 					= 	$params->get('adminEmail');
+		
+		$item->str_from_email 					= 	$config->get( 'mailfrom' );
+    	$item->str_from_email_name				= 	$config->get( 'fromname' );
+		$item->id								=	$user->id;
+		
+		if($item->str_email)
+		{
+			$item->flg_email_valid 				=	$mdl_common->fnc_validate_email($item->str_email);
+			if($item->flg_use_catcha)
+			{
+				$item->flg_catcha_correct 		= 	$mdl_common->fnc_check_catcha($item->str_view);
+			}
+			else{
+				$item->flg_catcha_correct = 1;
+			}
+		}
+		if(($item->flg_catcha_correct)and($item->flg_email_valid))
+		{
+			$mdl_default->_buildQuery_UpdateUser($item->str_email,$item->flg_send_reading,$item->flg_send_verse);
+		}
 		//Filters
-		$config	= JComponentHelper::getParams( 'com_zefaniabible' );
-		$user = JFactory::getUser();
-		$this->assignRef('user',				$user);
-		$this->assignRef('access',		$access);
-		$this->assignRef('lists',		$lists);
-		$this->assignRef('config',		$config);
+		$this->assignRef('item',$item);
 		parent::display($tpl);
 	}
 }
