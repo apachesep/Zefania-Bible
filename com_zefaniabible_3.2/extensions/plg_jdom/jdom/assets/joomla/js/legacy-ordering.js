4b8706d160da60e1a4a9d86b3fe89fd4c648d408
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2015-01-07 12:50:09 -0500

    Removed 3.2 Version

--- com_zefaniabible_3.2/extensions/plg_jdom/jdom/assets/joomla/js/legacy-ordering.js
+++
@@ -1,9 +0,0 @@
-Joomla.tableOrdering=function(a,b,c,d)
-{
-	"undefined"===typeof d&&(d=document.getElementById("adminForm"));
-	
-	d.filter_order.value=a;
-	d.filter_order_Dir.value=b;
-
-	Joomla.submitform(c,d)
-};
