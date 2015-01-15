7fa74408ed7fb8c7ca4999b75a64fef31e2e443b
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2015-01-15 07:40:56 -0500

    Layout Adjusted

--- com_zefaniabible/admin/views/zefaniabibledictdetailitem/tmpl/edit.php
+++ com_zefaniabible/admin/views/zefaniabibledictdetailitem/tmpl/edit.php
@@ -26,19 +26,19 @@ JHtml::_('formbehavior.chosen', 'select');
 <form action="<?php JRoute::_('index.php?option=com_zefaniabible&id=' . (int)$this->item->id); ?>" method="post" name="adminForm" id="zefaniabibledictdetailitem-form" class="form-validate">
 	
 	<div class="form-inline form-inline-header">
-		<div class="control-group">
-			<div class="control-label"><?php echo $this->form->getLabel('dict_id'); ?></div>
-			<div class="controls"><?php echo $this->form->getInput('dict_id'); ?></div>
-		</div>
 	</div>
 
 	<div class="form-horizontal">
 	<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
 
-		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', 'ZefaniabibleDictDetailItem', $this->item->id, true); ?>
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', JText::_('ZEFANIABIBLE_LAYOUT_DETAILS'), $this->item->id, true); ?>
 		<div class="row-fluid">
 			<div class="span9">
 				<div class="row-fluid form-horizontal-desktop">			
+            <div class="control-group">
+                <div class="control-label"><?php echo $this->form->getLabel('dict_id'); ?></div>
+                <div class="controls"><?php echo $this->form->getInput('dict_id'); ?></div>
+            </div>                
 			<div class="control-group">
 				<div class="control-label"><?php echo $this->form->getLabel('item'); ?></div>
 				<div class="controls"><?php echo $this->form->getInput('item'); ?></div>
