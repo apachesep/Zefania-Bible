a0ea92e327e5e90f344a2418d722936a3e771813
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2015-01-15 07:39:26 -0500

    Moved field

--- com_zefaniabible/admin/views/zefaniacrossrefitem/tmpl/edit.php
+++ com_zefaniabible/admin/views/zefaniacrossrefitem/tmpl/edit.php
@@ -26,10 +26,7 @@ JHtml::_('formbehavior.chosen', 'select');
 <form action="<?php JRoute::_('index.php?option=com_zefaniabible&id=' . (int)$this->item->id); ?>" method="post" name="adminForm" id="zefaniacrossrefitem-form" class="form-validate">
 	
 	<div class="form-inline form-inline-header">
-		<div class="control-group">
-			<div class="control-label"><?php echo $this->form->getLabel('book_id'); ?></div>
-			<div class="controls"><?php echo $this->form->getInput('book_id'); ?></div>
-		</div>
+
 	</div>
 
 	<div class="form-horizontal">
@@ -39,6 +36,10 @@ JHtml::_('formbehavior.chosen', 'select');
 		<div class="row-fluid">
 			<div class="span9">
 				<div class="row-fluid form-horizontal-desktop">			
+            <div class="control-group">
+                <div class="control-label"><?php echo $this->form->getLabel('book_id'); ?></div>
+                <div class="controls"><?php echo $this->form->getInput('book_id'); ?></div>
+            </div>                
 			<div class="control-group">
 				<div class="control-label"><?php echo $this->form->getLabel('chapter_id'); ?></div>
 				<div class="controls"><?php echo $this->form->getInput('chapter_id'); ?></div>
