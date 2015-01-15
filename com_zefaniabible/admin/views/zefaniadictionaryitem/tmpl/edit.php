36254580db43fe8a6e9262dcb564a867f1a46d8c
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2015-01-15 07:41:46 -0500

    Dictionary upload button added

--- com_zefaniabible/admin/views/zefaniadictionaryitem/tmpl/edit.php
+++ com_zefaniabible/admin/views/zefaniadictionaryitem/tmpl/edit.php
@@ -11,6 +11,13 @@ defined("_JEXEC") or die("Restricted access");
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
 JHtml::_('formbehavior.chosen', 'select');
+jimport( 'joomla.filesystem.folder' );
+
+//get the zefaniabibleitem
+$model			= $this->getModel();
+$mld_dictionary	= $model->getItem();
+$isNew			= ($mld_dictionary->id < 1);
+$params	= JComponentHelper::getParams( 'com_zefaniabible' );
 ?>
 
 <script type="text/javascript">
@@ -45,7 +52,39 @@ JHtml::_('formbehavior.chosen', 'select');
 				<div class="row-fluid form-horizontal-desktop">			
 			<div class="control-group">
 				<div class="control-label"><?php echo $this->form->getLabel('xml_file_url'); ?></div>
-				<div class="controls"><?php echo $this->form->getInput('xml_file_url'); ?></div>
+				<div class="controls">
+					<?php if($isNew){?>
+						<?php 
+                            $str_bibles_path = $params->get('xmlDictionaryPath', 'media/com_zefaniabible/dictionary/');
+							$arr_file_list_bible = JFolder::files(JPATH_SITE.'/'.$str_bibles_path,'.xml');
+							$str_file_list = '';
+							
+							for($x = 0; $x < count($arr_file_list_bible); $x++)
+							{
+								$str_file_list .= '<option value="'.$arr_file_list_bible[$x].'">'.$arr_file_list_bible[$x].'</option>'.PHP_EOL;
+							}
+                        ?>
+                        <div class="input-prepend input-append">
+                            <div id="jform_xml_file_url_icon" class="btn add-on icon-checkmark" onclick="toggleElement('jform_xml_file_url','jform_xml_file_url_list');"> </div>
+                                <input name="jform[xml_file_url]" id="jform_xml_file_url" class="bible_input" value="<?php echo $this->item->xml_file_url; ?>" type="text">
+                        </div>
+                        <br />
+                        <div class="input-prepend input-append">
+                                <div id="jform_xml_file_url_list_icon" class="btn add-on icon-cancel" onclick="toggleElement('jform_xml_file_url_list','jform_xml_file_url');"> </div>
+                                <select name="jform[xml_file_url_list]" id="jform_xml_file_url_list" class="bible_input" ><?php echo $str_file_list; ?></select>
+                        </div>
+                        <div id="infoUpload1" class="intend">
+                            <span id="btnUpload1"></span>
+                            <button id="btnCancel1" type="button" onclick="cancelQueue(upload1);" class="ss-hide upload_button" disabled="disabled">Cancel</button>
+                            <span id="biblepathinfo" class="pathinfo ss-hide hasTip" title="<?php echo JText::_('ZEFANIABIBLE_FIELD_XML_UPLOAD_UPLOADINFO_TOOLTIP'); ?>">
+                                    <?php echo JText::_('ZEFANIABIBLE_FIELD_XML_UPLOAD_UPLOADINFO').' /'.trim($str_bibles_path, '/').'/'; ?>
+                            </span>
+                        </div>
+					<?php }else{?>
+                    	<?php echo $this->form->getInput('xml_file_url'); ?>
+                    <?php }?>                
+				
+                </div>
 			</div>
 				</div>
 			</div>
