c9235c58b8777f4fc2c97906a5a168e61e56afaa
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2015-02-02 14:56:10 -0500

    updated update SQL scripts

--- com_zefaniabible/admin/sql/updates/4.0.0.sql
+++ com_zefaniabible/admin/sql/updates/4.0.0.sql
@@ -1,1 +1,74 @@
+ALTER TABLE `#__zefaniabible_bible_names` CHANGE publish published tinyint(3);
+ALTER TABLE `#__zefaniabible_bible_names` (
+	ADD `checked_out` int(11) ,
+	ADD `checked_out_time` datetime  ,
+	ADD `created` datetime  ,
+	ADD `created_by` int(11) ,
+	ADD `modified` datetime  ,
+	ADD `modified_by` int(11) ,
+	ADD `access` int(11) ,
+	ADD `language` char(7) ,
+	ADD `metadata` text ,
+	ADD `metakey` text ,
+	ADD `metadesc` text
+);
+
+ALTER TABLE `#__zefaniabible_dictionary_info` CHANGE publish published tinyint(3);
+ALTER TABLE `#__zefaniabible_dictionary_info` (
+	ADD `checked_out` int(11) unsigned  DEFAULT '0',
+	ADD `checked_out_time` datetime  DEFAULT '0000-00-00 00:00:00',
+	ADD `created` datetime  DEFAULT '0000-00-00 00:00:00',
+	ADD `created_by` int(11) unsigned  DEFAULT '0',
+	ADD `modified` datetime  DEFAULT '0000-00-00 00:00:00',
+	ADD `modified_by` int(11) unsigned  DEFAULT '0',
+	ADD `access` int(11) unsigned  DEFAULT '0',
+	ADD `language` char(7)  COMMENT 'The language code for the article.',
+	ADD `metadata` text ,
+	ADD `metakey` text ,
+	ADD `metadesc` text
+);
+
+ALTER TABLE `#__zefaniabible_zefaniacomment` CHANGE publish published tinyint(3);
+ALTER TABLE `#__zefaniabible_zefaniacomment` (
+	ADD `checked_out` int(11) unsigned  DEFAULT '0',
+	ADD `checked_out_time` datetime  DEFAULT '0000-00-00 00:00:00',
+	ADD `created` datetime  DEFAULT '0000-00-00 00:00:00',
+	ADD `created_by` int(11) unsigned  DEFAULT '0',
+	ADD `modified` datetime  DEFAULT '0000-00-00 00:00:00',
+	ADD `modified_by` int(11) unsigned  DEFAULT '0',
+	ADD `access` int(11) unsigned  DEFAULT '0',
+	ADD `language` char(7)  COMMENT 'The language code for the article.',
+	ADD `metadata` text ,
+	ADD `metakey` text ,
+	ADD `metadesc` text
+);
+
+ALTER TABLE `#__zefaniabible_zefaniareading` CHANGE publish published tinyint(3);
+ALTER TABLE `#__zefaniabible_zefaniareading`(
+	ADD `checked_out` int(11) ,
+	ADD `checked_out_time` datetime ,
+	ADD `created` datetime ,
+	ADD `created_by` int(11) ,
+	ADD `modified` datetime ,
+	ADD `modified_by` int(11) ,
+	ADD `access` int(11) ,
+	ADD `language` char(7) ,
+	ADD `metadata` text ,
+	ADD `metakey` text ,
+	ADD `metadesc` text
+);
+
+ALTER TABLE `#__zefaniabible_zefaniaverseofday` CHANGE publish published tinyint(3);
+ALTER TABLE `#__zefaniabible_zefaniaverseofday` (
+	ADD `checked_out` int(11),
+	ADD `checked_out_time` datetime,
+	ADD `created` datetime,
+	ADD `created_by` int(11),
+	ADD `modified` datetime,
+	ADD `modified_by` int(11),
+	ADD `access` int(11),
+);
+
+
+
 
