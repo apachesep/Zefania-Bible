c13627c68a5f4503428dd531cb642c1102c37200
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2015-03-21 07:55:50 -0400

    Added calendar view

---
+++ com_zefaniabible/site/views/calendar/tmpl/default.php
@@ -0,0 +1,160 @@
+<?php
+
+/**                               ______________________________________________
+*                          o O   |                                              |
+*                 (((((  o      <  Generated with Cook           (100% Vitamin) |
+*                ( o o )         |______________________________________________|
+* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
+* @version		1.6
+* @package		ZefaniaBible
+* @subpackage	Zefaniabible
+* @copyright	Missionary Church of Grace
+* @author		Andrei Chernyshev - www.missionarychurchofgrace.org - andrei.chernyshev1@gmail.com
+* @license		GNU/GPL
+*
+* /!\  Joomla! is free software.
+* This version may have been modified pursuant to the GNU General Public License,
+* and as distributed it includes or is derivative of works licensed under the
+* GNU General Public License or other free or open source software licenses.
+*
+*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
+*             (   )  (   )
+* -------------\ (----) /----------------------------------------------------------- +
+*               \_)  (_/
+*/
+defined('_JEXEC') or die('Restricted access');
+
+$cls_bible_calendar = new CalendarViewDefault($this->item);
+
+class CalendarViewDefault
+{
+	public function __construct($item)
+	{
+		require_once(JPATH_COMPONENT_SITE.'/helpers/common.php');
+		$mdl_common 	= new ZefaniabibleCommonHelper;
+				
+		$str_redirect_url = "index.php?option=com_zefaniabible&view=calendar&bible=".$item->str_Bible_Version."&plan=".$item->str_reading_plan;
+		$int_year_prev = $item->int_year;
+		$int_month_prev = $item->int_month - 1;
+		$int_year_next = $item->int_year;
+		$int_month_next = $item->int_month + 1;
+		if($item->int_month == 12)
+		{
+			$int_year_next = $item->int_year+1;
+			$int_month_next =  1;
+		}
+		if($item->int_month == 1)
+		{
+			$int_year_prev = $item->int_year-1;
+			$int_month_prev = 12;			
+		}
+		//print_r($item);
+		$str_redirect_prev_url = JRoute::_($str_redirect_url. "&year=".$int_year_prev."&month=".$int_month_prev);
+		$str_redirect_next_url = JRoute::_($str_redirect_url. "&year=".$int_year_next."&month=".$int_month_next);		
+
+		$y= 1;
+		
+		echo '<form action="'. JFactory::getURI()->toString().'" method="post" id="adminForm" name="adminForm">'.PHP_EOL;
+		echo '<div class="zef_legend">'.PHP_EOL;
+		echo '	<div class="zef_reading_label">'.JText::_('ZEFANIABIBLE_READING_PLAN').'</div>'.PHP_EOL;
+		echo '	<div class="zef_reading_plan">'.PHP_EOL;
+		echo '		<select name="plan" id="reading" class="inputbox" onchange="this.form.submit()">'.PHP_EOL;
+		echo 		$item->obj_reading_plan_dropdown;
+		echo '		</select>'.PHP_EOL;
+		echo '	</div>'.PHP_EOL;
+		echo '	<div style="clear:both"></div>  '.PHP_EOL;
+					
+		echo '	<div class="zef_bible_label">'. JText::_('ZEFANIABIBLE_BIBLE_VERSION').'</div>'.PHP_EOL;
+		echo '	<div class="zef_bible">'.PHP_EOL;
+		echo '		<select name="bible" id="bible" class="inputbox" onchange="this.form.submit()">'.PHP_EOL;
+		echo		$item->obj_bible_Bible_dropdown;
+		echo '		</select>'.PHP_EOL;
+		echo '	</div>'.PHP_EOL;
+		echo '	<div style="clear:both"></div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		
+		echo '<div class="zef_calendar">'.PHP_EOL;
+		echo '	<div class="zef_calendar_month_header">'.PHP_EOL;
+		echo '		<div class="zef_calendar_month_prev"><a href="'.$str_redirect_prev_url.'">'.$mdl_common->fnc_get_month_name($int_month_prev).'</a></div>'.PHP_EOL;
+		echo '		<div class="zef_calendar_month_name">'.$mdl_common->fnc_get_month_name($item->int_month).' '.$item->int_year.'</div>'.PHP_EOL;
+		echo '		<div class="zef_calendar_month_next"><a href="'.$str_redirect_next_url.'">'.$mdl_common->fnc_get_month_name($int_month_next).'</a></div>'.PHP_EOL;	
+		echo '		<div style="clear:both"></div>'.PHP_EOL;	
+		echo '	</div>'.PHP_EOL;
+		
+		
+		for($o = 0; $o < 7; $o++)
+		{
+			echo '	<div class="zef_calendar_day_names">'.$mdl_common->fnc_get_day_name($o).'</div>'.PHP_EOL;
+		}
+		echo '	<div style="clear:both"></div>'.PHP_EOL;			
+
+		$int_total_days = $item->int_month_begin_weekday + $item->int_month_max_days;
+		$int_calendar_rows = ceil($int_total_days/7);
+		echo '	<div class="zef_calendar_graph">'.PHP_EOL;
+		for($z = 0; $z < $int_calendar_rows; $z++)
+		{
+			echo '		<div class="zef_calendar_week">'.PHP_EOL;			
+			for($x = 0; $x < 7; $x++)
+			{
+				if(((($z == 0)and($x >= $item->int_month_begin_weekday))or($z > 0))and($y <= $item->int_month_max_days))
+				{
+					$t=1;
+					echo '			<div class="zef_calendar_day">'.$y.'<br><div class="zef_calendar_reading">';
+					foreach($item->arr_reading as $arr_reading)
+					{
+						if($arr_reading->day_number == ($item->int_day_diff+$y))
+						{
+							echo '<div class="zef_calendar_link_div"><a class="zef_calendar_link" title="'.JText::_('ZEFANIABIBLE_VERSE_READING_PLAN_OVERVIEW_CLICK_TITLE').'" href="'.JRoute::_("index.php?option=com_zefaniabible&view=reading&plan=".$item->str_reading_plan."&bible=".$item->str_Bible_Version."&day=".$arr_reading->day_number.'&Itemid='.$item->str_view_plan).'#'.$t.'" target="_self">';
+							echo $mdl_common->fnc_make_scripture_title_abbrev(
+									$arr_reading->book_id,  
+									$arr_reading->begin_chapter, 
+									$arr_reading->begin_verse, 
+									$arr_reading->end_chapter, 
+									$arr_reading->end_verse );
+							echo '</a></div>';
+							$t++;
+						}
+					}
+					if(($item->int_day_diff + $y) > $item->int_max_days )
+					{
+						$int_new_reading_days = ($item->int_day_diff + $item->int_month_max_days) - $item->int_max_days;
+						foreach($item->arr_reading2 as $arr_reading)
+						{
+							if($arr_reading->day_number == $y - ($item->int_month_max_days - $int_new_reading_days))
+							{
+								echo '<div class="zef_calendar_link_div"><a class="zef_calendar_link" title="'.JText::_('ZEFANIABIBLE_VERSE_READING_PLAN_OVERVIEW_CLICK_TITLE').'" href="'.JRoute::_("index.php?option=com_zefaniabible&view=reading&plan=".$item->str_reading_plan."&bible=".$item->str_Bible_Version."&day=".$arr_reading->day_number.'&Itemid='.$item->str_view_plan).'#'.$t.'" target="_self">';
+								echo $mdl_common->fnc_make_scripture_title_abbrev(
+										$arr_reading->book_id,  
+										$arr_reading->begin_chapter, 
+										$arr_reading->begin_verse, 
+										$arr_reading->end_chapter, 
+										$arr_reading->end_verse );
+								echo '</a></div>';
+								$t++;
+							}						
+						}
+					}
+					echo '</div></div>'.PHP_EOL;
+					$y++;
+				}
+				else
+				{
+					echo '<div class="zef_calendar_day_empty"></div>'.PHP_EOL;	
+				}
+				
+			}
+			echo '		</div>'.PHP_EOL;
+			echo '		<div style="clear:both"></div>'.PHP_EOL;
+		}
+		echo '	</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		echo '<input type="hidden" name="option" value="'. $item->str_option.'" />'.PHP_EOL;
+		echo '<input type="hidden" name="view" value="'. $item->str_view.'" />'.PHP_EOL;
+	    echo '<input type="hidden" name="Itemid" value="'. $item->int_menu_item_id.'"/>'.PHP_EOL;
+	    echo '<input type="hidden" name="month" value="'. $item->int_month.'"/>'.PHP_EOL;
+		echo '<input type="hidden" name="year" value="'. $item->int_year.'"/>'.PHP_EOL;
+	echo '</form>'.PHP_EOL;
+	}
+	
+}
+?>             
---
+++ com_zefaniabible/site/views/calendar/view.html.php
@@ -0,0 +1,137 @@
+<?php
+
+/**                               ______________________________________________
+*                          o O   |                                              |
+*                 (((((  o      <  Generated with Cook           (100% Vitamin) |
+*                ( o o )         |______________________________________________|
+* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
+* @version		1.6
+* @package		ZefaniaBible
+* @subpackage	Zefaniabible
+* @copyright	Missionary Church of Grace
+* @author		Andrei Chernyshev - www.missionarychurchofgrace.org - andrei.chernyshev1@gmail.com
+* @license		GNU/GPL
+*
+* /!\  Joomla! is free software.
+* This version may have been modified pursuant to the GNU General Public License,
+* and as distributed it includes or is derivative of works licensed under the
+* GNU General Public License or other free or open source software licenses.
+*
+*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
+*             (   )  (   )
+* -------------\ (----) /----------------------------------------------------------- +
+*               \_)  (_/
+*/
+
+
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+/**
+ * HTML View class for the Zefaniabible component
+ *
+ * @static
+ * @package		Joomla
+ * @subpackage	Zefaniabible
+ *
+ */
+class ZefaniabibleViewCalendar extends JViewLegacy
+{
+	/*
+	 * Define here the default list limit
+	 */
+	protected $_default_limit = null;
+
+	function display($tpl = null)
+	{
+		$app = JFactory::getApplication();
+		$config = JFactory::getConfig();
+		$layout = $this->getLayout();
+		switch($layout)
+		{
+			case 'default':
+				$fct = "display_" . $layout;
+				$this->$fct($tpl);
+				break;
+		}
+	}
+	function display_default($tpl = null)
+	{
+		/*
+			Standard Bible
+			a = bible
+			b = book
+			c = chapter
+			com = commentary
+			dict = Dictionary
+			strong = Show/Hide Strong Numgers flag
+		*/		
+		$app = JFactory::getApplication();
+		$params = JComponentHelper::getParams( 'com_zefaniabible' );
+		$int_new_reading_days = 0;
+												
+		require_once(JPATH_COMPONENT_SITE.'/models/default.php');
+		require_once(JPATH_COMPONENT_SITE.'/helpers/common.php');
+		$mdl_default 	= new ZefaniabibleModelDefault;
+		$mdl_common 	= new ZefaniabibleCommonHelper;
+		
+		// menu item overwrites
+		$menuitemid = JRequest::getInt( 'Itemid' );
+		if ($menuitemid)
+		{
+			$menu = JFactory::getApplication()->getMenu();
+			$menuparams = $menu->getParams( $menuitemid );
+			$params->merge( $menuparams );
+		}		
+
+		$jinput = JFactory::getApplication()->input;
+
+		$item = new stdClass();				
+		$item->int_current_year 				= date("Y");
+		$item->int_current_month 				= date("n");
+		$item->int_current_month_max_days 		= date("t");
+		$item->int_current_week_day 			= date("w");
+		$item->int_current_day 					= date("j");
+		$item->int_current_month_begin_weekday 	= date("w", mktime(0, 0, 0, $item->int_current_month, 1, $item->int_current_year));
+
+		$item->str_primary_bible 				= 	$params->get('primaryBible', $mdl_default->_buildQuery_first_record());	
+		$item->str_primary_reading 				= 	$params->get('primaryReading', $mdl_default->_buildQuery_first_plan());
+		$item->flg_show_credit 					= 	$params->get('show_credit','0');
+		$item->str_start_reading_date 			= 	$params->get('reading_start_date', '1-1-2012');
+		
+		$item->int_menu_item_id 				= 	$jinput->get('Itemid', null, 'INT');
+		$item->str_option						= 	$jinput->get('option', null, 'CMD');
+		$item->str_com 							= 	$jinput->get('com', null, 'CMD'); 		
+		$item->str_tmpl 						= 	$jinput->get('tmpl',null,'CMD');
+		$item->str_view 						= 	$jinput->get('view', 'standard', 'CMD');
+		
+		$item->int_month 						= 	$jinput->get('month', $item->int_current_month, 'INT');
+		$item->int_year 						= 	$jinput->get('year', $item->int_current_year, 'INT');
+		$item->str_Bible_Version 				= 	$jinput->get('bible', $item->str_primary_bible, 'CMD');	
+		$item->str_reading_plan 				= 	$jinput->get('plan', $item->str_primary_reading,'CMD');	
+		
+		$item->arr_reading_plan_list			= 	$mdl_default->_buildQuery_reading_plan_list($item);
+		$item->arr_Bibles 						= 	$mdl_default->_buildQuery_Bibles_Names();
+		$item->int_max_days						=  	$mdl_default->_buildQuery_max_reading_days($item->str_reading_plan);
+		
+		$item->int_day_diff						= 	$mdl_common->fnc_calcualte_day_diff_custom($item->str_start_reading_date, $item->int_max_days, $item->int_month, $item->int_year, 1);		
+		$item->obj_reading_plan_dropdown		=	$mdl_common->fnc_reading_plan_drop_down($item);
+		$item->obj_bible_Bible_dropdown			= 	$mdl_common->fnc_bible_name_dropdown($item->arr_Bibles,$item->str_Bible_Version);				
+
+		
+		$item->int_month_begin_weekday			= 	date("w", mktime(0, 0, 0, $item->int_month, 1, $item->int_year));
+		$item->int_month_max_days				= 	date("t", mktime(0, 0, 0, $item->int_month, 1, $item->int_year));
+		
+		$item->str_view_plan					=	$mdl_default->_buildQuery_get_menu_id('reading');
+		$item->arr_reading						= 	$mdl_default->_buildQuery_readingplan_calendar($item->str_reading_plan, $item->int_day_diff, $item->int_month_max_days);
+
+		if(($item->int_day_diff + $item->int_month_max_days) > $item->int_max_days )
+		{
+			$int_new_reading_days = ($item->int_day_diff + $item->int_month_max_days) - $item->int_max_days;
+			$item->arr_reading2					=	$mdl_default->_buildQuery_readingplan_calendar($item->str_reading_plan, 0, $int_new_reading_days);
+		}	
+		$this->assignRef('item', 		$item);
+		parent::display($tpl);
+	}
+}
