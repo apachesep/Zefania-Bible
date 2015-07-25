6f199a4157506caf86b56268f195faf125fd252c
Andrei Chernyshev <andrei.chernyshev1@gmail.com>
2015-07-25 08:54:47 -0400

    No Changes

--- com_zefaniabible/admin/models/zefaniacomment.php
+++ com_zefaniabible/admin/models/zefaniacomment.php
@@ -49,7 +49,7 @@ class ZefaniabibleModelZefaniacomment extends JModelList
 	 * This method should only be called once per instantiation and is designed
 	 * to be called on the first call to the getState() method unless the model
 	 * configuration flag to ignore the request is set.
-	 *
+	 *
 	 * Note. Calling getState in this method will result in recursion.
 	 *
 	 * @param   string  $ordering   An optional ordering field.
